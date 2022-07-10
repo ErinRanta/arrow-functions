652a2cfd418a7498927c48d8dda7a3991fccbcae
Erin Ranta <erinran99@gmail.com>
2022-07-09 21:14:55 -0400

    completed 11th problem

--- app.js
+++ app.js
@@ -119,31 +119,38 @@ let sum = (a,b,c,d) => a + b + c + d;
 console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
+// let objectLit = function() {
+//   return {
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3',
+//   };
+// };
+
+let objectLit = () => ({key1: 'value1', key2: 'value2', key3: 'value3' });
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
-  let sum = a + b;
-  let product = a * b;
-  return [sum, product];
-};
+// let sumAndProduct = function(a, b) {
+//   let sum = a + b;
+//   let product = a * b;
+//   return [sum, product];
+// };
+
+let sumAndProduct = (a,b) => [a+b, a*b];
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
 let message = function(name) {
   return `Hello, ${name}!`;
 };
+
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(message('Allie'));
@@ -155,11 +162,14 @@ let Student = function(name, age, hometown) {
   this.hometown = hometown;
 };
 
+//let Student = (name, age, hometown) => {this.name = name; this.age = age; this.hometown = hometown;};
+
+
 let joe = new Student('Joe Schmoe', 100, 'Anytown, USA');
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -169,20 +179,24 @@ Student.prototype.greeting = function() {
   return `Hi, my name is ${this.name}`;
 };
 
+//Student.prototype.greeting = () => `Hi, my name is ${this.name}`;
+
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
+
+Student.courseName = () => 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -193,17 +207,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//The Joe Shmoe student Object from constructor
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//Since arrow is part of it, 'this' is referring to the global object.
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//Now that arrow functions are scoped globally, this will refer to the global object, whereas this in the constructor will refer to the local.
